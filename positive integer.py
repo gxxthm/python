1ccbeb5ff98e8eb913c946ec1fde3071046e862a
gxxthm <126661318+gxxthm@users.noreply.github.com>
2023-09-28 20:29:46 +0530

    Add files via upload

---
+++ positive integer.py
@@ -0,0 +1,13 @@
+n=int(input("enter the no of numbers in the list "))
+x=[]
+
+for i in range (n):
+    z=int(input("enter the no "))
+    x.append(z)
+for i in range (0,n):
+    if x[i]>0:
+        print(x[i])
+
+    else:
+        pass
+    
